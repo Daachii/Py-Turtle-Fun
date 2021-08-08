362116e07f48ce6aa113f028efcf0a078cb4e201
Dachi <88577990+Daachii@users.noreply.github.com>
2021-08-08 23:57:34 +0200

    Add files via upload

---
+++ here/BeatsLogo.py
@@ -0,0 +1,25 @@
+from turtle import *
+
+pensize(7)
+
+pu()
+goto(0, -215)
+pd()
+
+color('#ed1c24', '#ed1c24')
+begin_fill()
+circle(215)
+end_fill()
+
+pu()
+goto(-85, 215)
+pd()
+
+pensize(50)
+
+color('white')
+seth(-90)
+fd(215)
+circle(85)
+
+done()
