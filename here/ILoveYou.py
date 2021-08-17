79d72c273a0ee506b0d1427e98739575c576be9d
Dachi <88577990+Daachii@users.noreply.github.com>
2021-08-17 21:35:41 +0200

    Add files via upload

---
+++ here/ILoveYou.py
@@ -0,0 +1,27 @@
+from turtle import *
+
+pensize(7)
+
+color('#D21404', '#D21404')
+
+begin_fill()
+
+pu()
+goto(0, 100)
+pd()
+
+seth(60)
+circle(-90, 200)
+circle(1000, 11.131)
+
+pu()
+goto(0, 100)
+pd()
+
+seth(120)
+circle(90, 200)
+circle(-1000, 11.131)
+
+end_fill()
+
+done()
