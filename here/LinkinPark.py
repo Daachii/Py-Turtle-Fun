9ca13bd96b392e2a68986a7f2943b24aad2ca54e
Dachi <88577990+Daachii@users.noreply.github.com>
2021-08-20 12:56:24 +0200

    Add files via upload

---
+++ here/LinkinPark.py
@@ -0,0 +1,52 @@
+from turtle import *
+
+begin_fill()
+
+pu()
+goto(-70, -230)
+pd()
+
+seth(-17)
+circle(240, 190)
+
+seth(-117)
+fd(380)
+
+seth(0)
+fd(295)
+
+seth(120)
+fd(240)
+
+seth(-117)
+fd(309)
+
+seth(147)
+circle(-200, 140)
+
+seth(63)
+fd(48)
+
+seth(-179.5)
+circle(240, 157)
+
+seth(63)
+fd(272.5)
+
+seth(-60)
+fd(100)
+
+seth(180)
+fd(170)
+
+seth(63)
+fd(292)
+
+seth(-13)
+circle(-200, 181.5)
+
+goto(-70, -230)
+
+end_fill()
+
+done()
